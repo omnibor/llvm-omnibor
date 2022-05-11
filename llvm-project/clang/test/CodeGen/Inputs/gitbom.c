d70eb0a8aaeec88893c76ea5e0b3b26eb9d5d1f0
bharsesh <bharathi.seshadri@gmail.com>
2022-05-11 17:42:48 +0000

    Remove llvm 13.0.0
    Signed-off-by: bharsesh <bharathi.seshadri@gmail.com>

--- llvm-project/clang/test/CodeGen/Inputs/gitbom.c
+++
@@ -1,4 +0,0 @@
-#include "gitbom.h"
-int add(int x, int y) {
-  return x + y;
-}
--- llvm-project/clang/test/CodeGen/gitbom_test.c
+++
@@ -1,7 +0,0 @@
-// RUN: rm -rf %t && mkdir %t
-// RUN:  %clang -c -frecord-gitbom -MD -o %t/gitbom.o %S/Inputs/gitbom.c -I%S/Inputs/gitbom.h
-// RUN: llvm-readelf -p ".bom" %t/gitbom.o | FileCheck --check-prefix=BOM_IDENTIFIER %s
-// RUN: cat %t/.gitbom/object/50/16da29beed4d19143174ae53d5a69d5fa2afa4 | FileCheck --check-prefix=BOM_FILE_CONTENTS %s
-// BOM_IDENTIFIER: [     0] P..)..M..1t.S..._...
-// BOM_FILE_CONTENTS: blob 6e00bf3ad31d164ffc9a1a64a377857607a24085
-// BOM_FILE_CONTENTS: blob c7184931bb3205d7eff290af1860fbed5f963fb5
